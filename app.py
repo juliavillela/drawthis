ce5302d234dee88bf31b9d6bf79a9dbaa74c2bdf
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-08-14 18:49:47 -0300

    can add to db from admin route

--- app.py
+++ app.py
@@ -27,10 +27,25 @@ def index():
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
     if request.method == "GET":
-        table_names = db.tables
+        #if user selected table, display table, else, display only table links
         selected_table = request.args.get("table")
+        table_names = db.tables
         if selected_table:
             table = db.get_all_from(selected_table)
         else:
             table = None
         return render_template("admin.html", table_names = table_names, table = table)
+
+    else:
+        table_name = request.form.get("table_name")
+        table_keys = db.get_keys(table_name)
+        keys = []
+        data = []
+        for key in table_keys:
+            input = request.form.get(key['name'])
+            if input:
+                keys.append(key['name'])
+                data.append("'" + input + "'")
+        db.insert(keys, data, table_name)
+
+        return redirect("/admin")
--- db.py
+++ db.py
@@ -13,11 +13,22 @@ class DB:
             tables.append(data)
         return tables
 
+    def get_keys(self, table_name):
+        keys = self.db.execute("SELECT name FROM PRAGMA_TABLE_INFO('" + table_name + "');")
+        return keys
+
     def get_all_from(self, table_name):
         table = {'table_name':table_name, 'keys': None, 'data':[]}
-        table['keys'] = self.db.execute("SELECT name FROM PRAGMA_TABLE_INFO('" + table_name + "');")
+        table['keys']= self.db.execute("SELECT name FROM PRAGMA_TABLE_INFO('" + table_name + "');")
         data = self.db.execute("SELECT * FROM " + table_name)
         for r in data:
             row = r.values()
             table['data'].append(row)
         return table
+
+    def insert(self, keys, data, table_name):
+        separator = ", "
+        keys_string = separator.join(keys)
+        data_string = separator.join(data)
+        query = "INSERT INTO " + table_name + " (" + keys_string + ") VALUES (" + data_string + " );"
+        self.db.execute(query)
