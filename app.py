37d622204a97e00d78261b29eb0883ceed407c59
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-08-14 14:10:15 -0300

    basic setup of  flask app and index route

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from cs50 import SQL
+
+# Configure application
+app = Flask(__name__)
+
+#assign db source
+db = SQL("sqlite:///drawthis.db")
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#routes
+@app.route("/")
+def index():
+    return render_template("index.html")
