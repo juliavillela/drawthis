ddb11898d64d9c003f10c4711636151b89b85171
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-09-11 21:42:54 -0300

    helper file with formatting db rules

---
+++ format.py
@@ -0,0 +1,45 @@
+import re
+
+#takes in a string formated as array
+#and returns clean array of strings
+def str_arr(str_arr):
+    if str_arr:
+        csv = str_arr
+        for c in ["'", "[", "]"]:
+            csv = csv.replace(c, "")
+        array = csv.split(",")
+        return array
+    else:
+        return []
+
+#joins an array of strings in db format
+def join(array):
+    if array:
+        s = "%"
+        return s.join(array)
+    else:
+        return ""
+
+#splits db string into array of strings
+def split(string):
+    if string:
+        array = string.split("%")
+        return array
+    else:
+        return []
+
+def cards_imd(imd):
+    cards =[]
+    for item in imd:
+        if re.search("card", item):
+            cards.append(imd.get(item))
+    string = join(cards)
+    return string
+
+def filter_in_text(string):
+    for old, new in [("'", "''")]:
+        string.replace(old, new)
+
+def filter_out_text(string):
+    for old, new in [("'", "''")]:
+        string.replace(old, new)
